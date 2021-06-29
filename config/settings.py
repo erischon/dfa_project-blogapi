4783b7ff3f6f2a3e2b45cd818faeffcb5a352b0c
Eri Schon <erischon@gmail.com>
2021-06-29 10:59:00 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 3rd party
 	'rest_framework',
+    'rest_framework.authtoken',
 
     # Local
     'posts',
@@ -48,7 +49,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
